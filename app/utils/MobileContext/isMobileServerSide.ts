ddbc5abb1f45fb10565dd22b0181dbc940b4d10e
Danil <litan.yt.terr@gmail.com>
2024-12-16 22:30:29 +0700

    new versions

--- app/utils/MobileContext/isMobileServerSide.ts
+++ app/utils/MobileContext/isMobileServerSide.ts
@@ -4,7 +4,7 @@ const isMobile = (userAgent: string | null | undefined): boolean => {
   return /Mobi|Android/i.test(userAgent || '');
 };
 
-export const isMobileServerSide = (): boolean => {
-  const userAgent: string | null = headers().get('user-agent');
+export const isMobileServerSide = async (): Promise<boolean> => {
+  const userAgent = await headers().then((header) => header.get('user-agent'));
   return isMobile(userAgent);
 };
